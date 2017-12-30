179dbd91f595512883efefa0a62eee3026aff350
Amit Dubey <amit.dubey@gmail.com>
2017-12-30 00:14:21 -0800

    Added training script

---
+++ bin/train.sh
@@ -0,0 +1,5 @@
+TROVE_JAR=/Users/adubey/src/music/lib/trove-2.0.2.jar
+MALLET_JAR=/Users/adubey/src/music/lib/mallet.jar
+MUSIC_JAR=/Users/adubey/src/music/target/scala-2.11/music_2.11-1.0.jar
+JARS="$TROVE_JAR":"$MALLET_JAR":"$MUSIC_JAR"
+~/Downloads/scala-2.11.8/bin/scala -cp "$JARS" ca.dubey.music.percussion.Train2 $@
