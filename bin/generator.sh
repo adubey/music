2fd9969f6820766aa713c1af94b8be854daf62bc
Amit Dubey <amit.dubey@gmail.com>
2018-01-01 09:43:35 -0800

    Take args from command line

--- bin/generator.sh
+++ bin/generator.sh
@@ -2,4 +2,4 @@ TROVE_JAR=/Users/adubey/src/music/lib/trove-2.0.2.jar
 MALLET_JAR=/Users/adubey/src/music/lib/mallet.jar
 MUSIC_JAR=/Users/adubey/src/music/target/scala-2.11/music_2.11-1.0.jar
 JARS="$TROVE_JAR":"$MALLET_JAR":"$MUSIC_JAR"
-~/Downloads/scala-2.11.8/bin/scala -cp "$JARS" ca.dubey.music.percussion.Generator model3
+~/Downloads/scala-2.11.8/bin/scala -cp "$JARS" ca.dubey.music.percussion.Generator $@
