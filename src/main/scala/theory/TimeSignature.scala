3f71b379b6576f5fd0256d6a2b87ae64cfb5d2ce
Amit Dubey <amit.dubey@gmail.com>
2018-01-01 09:59:12 -0800

    Added measure-level extraction

--- src/main/scala/theory/TimeSignature.scala
+++ src/main/scala/theory/TimeSignature.scala
@@ -1,4 +1,8 @@
 package ca.dubey.music.theory
 
 class TimeSignature(val beatsPerMeasure : Int,
-                    val beatUnit : Int)
+                    val beatUnit : Int) {
+  def ticksPerMeasure(ticksPerQuarterNote : Int) : Int = {
+    return ticksPerQuarterNote / (beatUnit/4) * beatsPerMeasure
+  }
+}
